a942d617bdc6f5385361680c4852ba21e7859f6f
Flavio Renzi <flavioinv@gmail.com>
2024-02-17 14:07:00 +0100

    Added cmake and config file

---
+++ HoeffdingTree/Config.h
@@ -0,0 +1,12 @@
+#ifndef HOEFFDINGTREE_CONFIG_H
+#define HOEFFDINGTREE_CONFIG_H
+
+
+#define CLASS_NUMBER 2
+#define FEATURE_NUMBER 2
+#define MIN_SAMPLES 10
+#define R 1.0
+#define DELTA 0.5
+
+
+#endif
--- HoeffdingTree/HoeffdingTree.h
+++ HoeffdingTree/HoeffdingTree.h
@@ -6,12 +6,7 @@
 #include <float.h>
 #include <math.h>
 #include <stdlib.h>
-
-#define CLASS_NUMBER 2
-#define FEATURE_NUMBER 2
-#define MIN_SAMPLES 10
-#define R 1.0
-#define DELTA 0.5
+#include "Config.h"
 
 typedef struct FeatureObserver{
     float mean;
